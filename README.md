1123a7fb8f35d6647b762613fd4b8eab229cce86
Robert Lieck <robert.lieck@gmail.com>
2019-08-25 17:23:17 +0200

    write documentation incl. example

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# bibtex_to_html
-Convert your LaTeX references to publishable website including links to PDF
+# Bibtex to HTML
+This script takes a `bibtex` file with links to local PDF files and creates a folder containing a static webpage and copies of the PDF files. The webpage contains a literature list in `LaTeX` style with additional links to the PDF files. You can simply copy over the folder to your webserver to publish your literature list.
+
+## Running the script
+
+Run the script by calling
+
+```python path/to/make.py literature```
+
+in a folder containing (modified) copies of `literature.tex`, `literature.bib`, and `config.json`. You need to have a working `python` and `LaTeX` installation for this to work. On Windows you will need to adapt `file_prefix` in `config.json` (see below).
+
+## Dependencies
+
+All dependencies should come with a basic `python` and `LaTeX` installation. Specifically, the following python packages are used
+
+```sys, os, pathlib, shutil, subprocess, re, json```
+
+and you will need `pdflatex`, `biblatex`, `biber`, and `tex4ht/htlatex`.
+
+## Workflow
+
+The script first compiles [literature.tex](literature.tex) using `pdflatex` and `biblatex` with `biber` backend by running
+
+1. `pdflatex`
+2. `pdflatex`
+3. `biber`
+4. `pdflatex`
+5. `pdflatex`
+
+to produce a literature list as `literature.pdf`. After that the `htlatex` command from the `tex4ht` package is used to generate an HTML version `literature.html`. The generated output contains special tags for the `file` fields in the `bibtex` entries.
+
+The HTML file is then post-processed to extract the actual file paths, copy the files in a separate folder and produce a copy of `literature.html` with relative links to these local files.
+
+**Example:** The file [literature.bib](literature.bib) is compiled to [literature.html](literature/literature.html).
+
+## Zotero integration
+
+If you are using `Zotero` with the `Better BibTeX` plugin, maintaining your literature list becomes particularly easy:
+
+1. add all items to a (sub-)collection
+2. add local PDF files to each item
+3. export (and optionally `keep updated`) using the `Better BibLaTeX` format.
+
+This will automatically add links to your local PDF files to the `bibtex` entries.
+
+## Configuration and tweaking
+
+### config.json
+
+If you do not want the full path (including you home directory etc.) to re-appear in the HTML you should adapting `file_prefix` by the real absolute path to your Zotero storage (or where ever your PDF files are stored). Also, on Windows the default value of `/` for the root directory will probably not work. The other values mean the following:
+
+```"split_token": "#[PDF]#"```
+
+- the token right before and after the file information, defined in `literature.tex`.
+
+```"encoding": "iso-8859-1"```
+
+- the encoding produced by `htlatex`, this is also specified in the HTML file and might be important if you encounter problem with special characters and the like
+
+```"file_link_start": "<a \nhref=\""```
+```"file_link_mid": "\" class=\"url\" ><span \nclass=\"cmtt-12\">"```
+```"file_link_end": "</span></a>"```
+
+- `htlatex` transforms the file information into a link with link text; this is the remaining HTML code in between the `split_token` separators, that are not just the file name
+
+```"pdf_dir": "pdfs"```
+
+- the subdirectory in which to copy the local PDF files
+
+### literature.tex
+
+`literature.tex` is a standard `LaTeX` file with some customisations for the `BibLaTeX` output, especially the different sections of the literature list. You can adapt it to your needs.
+
+### literature.bib
+
+`literature.bib` is a standard `bibtex` file with two exceptions:
+
+1. It should contain `file` field for PDFs to be linked. Multiple PDF files can be separated by a semicolon `;`, which is how Zotero exports multiple attachments.
+
+2. You can use the `author+an` field to specify highlighting of authors:
+
+   ```author+an = {1=highlight}```
+
+   will highlight the first author etc. In Zotero you can add this information by specifying
+
+   ```bibtex*{'author+an':'1=highlight'}```
+
+   in the `Extra` field.
+
---
+++ make.py
@@ -0,0 +1,122 @@
+import sys
+import os
+import pathlib
+import shutil
+import subprocess
+import re
+import json
+
+
+def compile_to_html(file_name_base):
+    pdflatex_command = ["pdflatex",
+                        "-file-line-error",
+                        # "-shell-escape",
+                        # "--synctex=1",
+                        "-interaction=nonstopmode",
+                        file_name_base]
+    biber_command = ["biber", file_name_base]
+    html_commend = ["htlatex", file_name_base]
+    subprocess.run(pdflatex_command)
+    subprocess.run(pdflatex_command)
+    subprocess.run(biber_command)
+    subprocess.run(pdflatex_command)
+    subprocess.run(pdflatex_command)
+    subprocess.run(html_commend)
+
+
+def post_process_html(file_name_base, cwd):
+    config = json.load(open('config.json'))
+    # remove and re-create output directory
+    rel_out_dir = file_name_base
+    abs_out_dir = os.path.join(cwd, rel_out_dir)
+    if os.path.isdir(abs_out_dir):
+        shutil.rmtree(abs_out_dir)
+    os.makedirs(abs_out_dir)
+    # read file
+    file_content = open(file_name_base + ".html", 'rb').read().decode(encoding=config['encoding'])
+    # open output file (same name but in output directory
+    out_file = open(os.path.join(abs_out_dir, file_name_base + ".html"), 'wb')
+    # convert to pathlib Path objects
+    rel_out_dir = pathlib.Path(rel_out_dir)
+    abs_out_dir = pathlib.Path(abs_out_dir)
+    # split at pdf tokens and replace
+    for idx, section in enumerate(file_content.split(config['split_token'])):
+        # only every second section corresponds to pdf
+        if idx % 2 == 0:
+            pass
+        # if section is empty ignore (remains empty)
+        elif section == '':
+            pass
+        else:
+            # get list of files from section
+            file_path_regex = re.compile('^' +
+                                         config['file_link_start'] +
+                                         '([^' + config['file_link_mid'][0] +
+                                         ']+)' +
+                                         config['file_link_mid'] +
+                                         '([^' +
+                                         config['file_link_end'][0] +
+                                         ']+)' +
+                                         config['file_link_end'] +
+                                         '$')
+            file_list = file_path_regex.search(section).group(1).split(";")
+            # fill section with new content
+            section = ""
+            # go through files
+            for file in file_list:
+                if not file.endswith(".pdf"):
+                    # ignore non-pdf files
+                    continue
+                if section == "":
+                    # open bracket on first entry
+                    section = "["
+                else:
+                    # add space between entries
+                    section += " "
+                # get paths for copying and referencing pdfs
+                orig_file_path = pathlib.Path(file)
+                prefix = pathlib.Path(config['file_prefix'])
+                if orig_file_path.parts[:len(prefix.parts)] != prefix.parts[:]:
+                    raise UserWarning(f"File prefix does not match\n"
+                                      f"    prefix: {prefix}\n"
+                                      f"    actual path: {orig_file_path}")
+                new_abs_file_path = pathlib.Path('/').joinpath(*abs_out_dir.parts[:],
+                                                               config['pdf_dir'],
+                                                               *orig_file_path.parts[len(prefix.parts):])
+                new_rel_file_path = pathlib.Path('.').joinpath(*rel_out_dir.parts[1:],
+                                                               config['pdf_dir'],
+                                                               *orig_file_path.parts[len(prefix.parts):])
+                # print(f"orig_file_path: {orig_file_path}")
+                # print(f"prefix: {prefix}")
+                # print(f"abs_out_dir: {abs_out_dir}")
+                # print(f"rel_out_dir: {rel_out_dir}")
+                # print(f"new_abs_file_path: {new_abs_file_path}")
+                # print(f"new_rel_file_path: {new_rel_file_path}")
+                # create containing directory and copy file
+                os.makedirs(new_abs_file_path.parent)
+                shutil.copyfile(orig_file_path, new_abs_file_path)
+                # add relative link to file
+                section += r'''<a href="''' + str(new_rel_file_path) + r'''">PDF</a>'''
+            if section != "":
+                # close bracket if any file was referenced
+                section += "]"
+        # write to out file
+        out_file.write(section.encode(encoding=config['encoding']))
+
+
+if __name__ == '__main__':
+    # check number of arguments
+    if len(sys.argv) < 2:
+        print("please specify file name without extension")
+        exit()
+    # get file names and check they actually exist
+    file_name_base = sys.argv[1]
+    tex_file = file_name_base + ".tex"
+    if not os.path.isfile(tex_file):
+        print(f"file '{tex_file}' does not exist")
+        exit()
+    # compile to html
+    compile_to_html(file_name_base)
+    # post-process html
+    post_process_html(file_name_base, os.getcwd())
+
